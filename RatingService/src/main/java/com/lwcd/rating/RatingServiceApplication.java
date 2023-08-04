d5ff4c83e67a39d01dad844086a68dc4391ee4ce
Vikrant Mahto <vikrant.mahto@juspay.in>
2023-08-04 09:01:46 +0530

    Adding Registry

--- RatingService/src/main/java/com/lwcd/rating/RatingServiceApplication.java
+++ RatingService/src/main/java/com/lwcd/rating/RatingServiceApplication.java
@@ -3,8 +3,11 @@ package com.lwcd.rating;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
