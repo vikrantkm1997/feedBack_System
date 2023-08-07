f6085b41ce7a704908c880a70d3fa115f6ce02e2
Vikrant Mahto <vikrant.mahto@juspay.in>
2023-08-08 00:58:30 +0530

    Enabled APiGateway Service

---
+++ ApiGateWay/src/test/java/com/lwcd/gateway/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwcd.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
